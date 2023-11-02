0973bc525d0d1a5e74784cfd08f10031926ff50b
Jun So <129551078+thatcantbeso@users.noreply.github.com>
2023-11-02 20:46:30 +0000

    fixed some lines

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -21,9 +21,7 @@ class CommentsController < ApplicationController
   def edit
     respond_to do |format|
       format.html
-      format.js do
-        render template: "comments/edit"
-      end
+      format.js
     end
   end
 
@@ -55,9 +53,7 @@ class CommentsController < ApplicationController
       if @comment.update(comment_params)
         format.html { redirect_to root_url, notice: "Comment was successfully updated." }
         format.json { render :show, status: :ok, location: @comment }
-        format.js do
-          render template: "comments/edit"
-        end
+        format.js
       else
         format.html { render :edit, status: :unprocessable_entity }
         format.json { render json: @comment.errors, status: :unprocessable_entity }
